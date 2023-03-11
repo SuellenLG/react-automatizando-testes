03d3fd247410efb0b61828bfc6132fb997de8099
Suellen Lopes Gualberto <suellenlopesgualberto@gmail.com>
2023-03-11 11:10:07 -0300

    Mock and assync code

---
+++ src/conta/Conta.test.js
@@ -0,0 +1,34 @@
+import React from  "react";
+import Conta from "./Conta" ;
+import { render, screen, fireEvent } from "@testing-library/react";
+
+
+describe('Componente de conta', () => {
+    it('Exibir o saldo da conta como valor monetário', () => {
+        render(<Conta saldo={1000} />)
+        
+        const saldo = screen.getByTestId('saldo-conta');
+
+        
+        expect(saldo.textContent).toBe('R$ 1000');
+    })
+
+    it('Chama a função de realizar transação, quando o botão é clicado', () => {
+        const funcaoRealizarTransação = jest.fn()
+    
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransação} />)
+    
+        fireEvent.click(screen.getByText('Realizar operação'));
+    
+        expect(funcaoRealizarTransação).toHaveBeenCalled()
+      })
+})
+
+
+
+  
+    
+    
+    
+
+    
